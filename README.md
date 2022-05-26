7f44f1cc8c3a9795a6d6eb52fa959fd64a29bc4d
ValentineRiedman <98363725+ValentineRiedman@users.noreply.github.com>
2022-05-25 21:59:52 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Part Time React Starter
+
+We'll use this as a springboard to explore the main facets of ReactJS:
+
+- components
+- expressions
+- JSON.stringify
+- input basics
+- -local state
+- conditional rendering
---
+++ src/components/App/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Dorian React Intro</h1>
+        </header>
+        <div>
+          <p>
+            body text
+          </p>
+        </div>
+      </div>
+    );
+}
+
+export default App;
